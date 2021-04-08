9cb83d9a789b2922da6d89bcfe12a59e60b7f1bd
cdiamond3 <diamond.chris.3@gmail.com>
2021-04-07 18:08:23 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const catscopy = [...cats, 'Broom'];
+    return catscopy;
+}
+
+function prependCat() {
+    const copycats = ["Arnold",...cats];
+    return copycats;
+}
+
+function removeLastCat() {
+    const newcats = cats.slice(0, cats.length -1);
+    return newcats;
+}
+
+function removeFirstCat() {
+    const lastcat = cats.slice(1);
+    return lastcat;
+}
